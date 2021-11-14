f7a2fdc7e6645c76f98ff6f43904b81be819d33f
ymtyroni <lukasz.calka@protonmail.com>
2021-11-14 10:35:10 +0100

    add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+
+if __name__ == "__main__":
+    # funkcja do minimalizacji: J =  sum_{k=0}^{N-1} sqrt(Uk)
+    # Ograniczenia:
+    # X{k+1} = ax{k} - u{k}
+    # X{0} = X{N}
+    # a = 1.1, X{0} = 100, N = 2,4,10,20,25
